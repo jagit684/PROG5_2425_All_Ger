e7b8bbcd75f2b431208bf112d187567d1529ed92
Ger Saris <gbj.saris@avans.nl>
2024-09-15 11:29:17 +0200

    wk1_HelloWorld added

---
+++ wk1_HelloWorld/wk1_HelloWorld/MyIO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wk1_HelloWorld
+{
+    /*
+     * This class is a simple input/output class that can be used to get input from the user.
+     */
+    internal class MyIO
+    {
+        public string? GetString(string prompt)
+        {
+            string? input;
+            Console.Write("> " + prompt);
+            input = Console.ReadLine();
+            return input;
+        }
+
+        public string GetNotEmptyString(string prompt)
+        {
+            string? input = null;
+            while (String.IsNullOrWhiteSpace(input))
+            {
+                Console.Write("> " + prompt);
+                input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("> incorrect");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return input;
+        }
+
+        public DateTime GetNotEmptyDate(string prompt)
+        {
+            string input = GetNotEmptyString(prompt);
+            while (!DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("> incorrect");
+                input = GetNotEmptyString(prompt);
+            }
+            return DateTime.ParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+
---
+++ wk1_HelloWorld/wk1_HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using MyDomain;
+using System.Globalization;
+
+namespace wk1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyIO io = new MyIO();
+            Console.WriteLine("Hello, please enter a person.");
+
+            string first = io.GetNotEmptyString("voornaam: ");
+            string? middle = io.GetString("tussenvoegsel: ");
+            string last = io.GetNotEmptyString("achternaam: ");
+            DateTime birthdate = io.GetNotEmptyDate("geboortedatum (dd-mm-yyyy): ");
+
+
+            Person p = new Person(first, middle, last, birthdate);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            char horizontalBar = '\u2015';
+            Console.WriteLine($"{new string(horizontalBar, 35)}");
+            Console.Write("Hallo " + p.FullName + ", ");
+            Console.WriteLine("je bent " + p.Age + " jaar en ");
+            Console.WriteLine((p.IsRetired ? "" : "niet ") + "met pensioen.");
+            Console.WriteLine($"{new string(horizontalBar, 35)}");
+        }
+    }
+}
